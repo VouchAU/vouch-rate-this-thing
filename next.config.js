b109c58639daa68d0c0546acdb1083b90073e37f
Jay Almaraz <jay.amgrant@gmail.com>
2022-11-23 12:15:20 +1000

    [tmp] Testing CSP + XFO headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+// const ContentSecurityPolicy = `
+//   default-src 'self';
+//   script-src 'self';
+//   child-src example.com;
+//   style-src 'self' example.com;
+//   font-src 'self';
+// `
+
+const ContentSecurityPolicy = `
+  frame-src 'self' pro.vouch.video;
+`;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
